4574f2c727ce1d97a8d3a215b6100b07734667de
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-03-04 09:11:45 -0500

    En proceso

---
+++ main.py
@@ -0,0 +1,20 @@
+import modules.getClients as cliente
+
+
+# import module
+from tabulate import tabulate
+ 
+# assign data
+mydata = [
+    ["Nikhil", "Delhi"], 
+    ["Ravi", "Kanpur"], 
+    ["Manish", "Ahmedabad"], 
+      ["Prince", "Bangalore"]
+]
+ 
+# create header
+head = ["Name", "City"]
+ 
+# display table
+print(tabulate(cliente.getAllClientPaisRegionCiudad("Spain", "Fuenlabrada", "Madrid")))
+ 
