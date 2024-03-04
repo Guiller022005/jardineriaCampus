210d934b628f8632b3762fe40acececa420e81f2
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-03-04 13:21:52 -0500

    feat: :construction: filtro de nombre y direccion
    buscar por nombre y direccion

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
 import modules.getClients as cliente
 
-
-# import module
-from tabulate import tabulate
- 
-# assign data
-mydata = [
-    ["Nikhil", "Delhi"], 
-    ["Ravi", "Kanpur"], 
-    ["Manish", "Ahmedabad"], 
-      ["Prince", "Bangalore"]
-]
- 
-# create header
-head = ["Name", "City"]
- 
-# display table
-print(tabulate(cliente.getAllClientPaisRegionCiudad("Spain", "Fuenlabrada", "Madrid")))
+print(cliente.getAllClientsDirreccion2("linea_direccion2"))
  
