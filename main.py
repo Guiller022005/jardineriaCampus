e7d7c3fe30527311a21843092cfa864b36de345d
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-03-11 09:18:34 -0500

    Nueva opcion salir

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import modules.getOficina as oficina
 import modules.getEmpleado as empleado
 import modules.getPedido as pedidos
 import modules.getPagos as pago
+import modules.getProducto as producto
 import sys
-
 if(__name__ == "__main__"):
         #https://patorjk.com/software/taag/#p=display&h=2&v=2&f=Slant&t=Men%C3%BA%20principal%0A1.%20cliente%0A2.%20oficina%0A3.%20empleado%0A4.%20pedidos%0A
+    while True:
         print("""
     __  ___             __                _            _             __
    /  |/  /__  ____  __/_/_   ____  _____(_)___  _____(_)___  ____ _/ /
@@ -22,15 +23,22 @@ if(__name__ == "__main__"):
                         3. Empleado
                         4. Pedidos
                         5. Pagos
+                        6. Productos
+                        7. Salir
 """)
-opcion = int(input("\nSeleccione una de las opciones"))
-if(opcion == 1):
-    cliente.menu()
-elif(opcion == 2):
-    oficina.menu()
-elif(opcion == 3):
-    empleado.menu()
-elif(opcion == 4):
-    pedidos.menu()
-elif(opcion == 5):
-    pago.menu()
+        opcion = int(input("\nSeleccione una de las opciones: "))
+        if(opcion == 1):
+            cliente.menu()
+        elif(opcion == 2):
+            oficina.menu()
+        elif(opcion == 3):
+            empleado.menu()
+        elif(opcion == 4):
+            pedidos.menu()
+        elif(opcion == 5):
+            pago.menu()
+        elif(opcion == 6):
+            producto.menu()
+        elif(opcion == 7):
+            break
+
