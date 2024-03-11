c4bc23773368596f5f7444ae7d8ccf972332fac8
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-03-11 15:53:34 -0500

    feat: :construction: Se crearon nuevos filtros y se añadieron al menu
    Añadimos la opcion producto al menu y diseñamos nuevas funciones

--- modules/getProducto.py
+++ modules/getProducto.py
@@ -15,18 +15,51 @@ def getAllStocksPriceGama(gama, stock):
         return val.get("precio_venta")
     condiciones.sort(key=price)
     return condiciones
-
+# Obtener la gama, nombre, codigo producto, precio de venta
 def getAllGamaCodigoNombre():
     producto =[]
     for val in pr.producto:
         
             producto.append({
-                "codigo_cliente": val.get('codigo_cliente'),
-                "nombre_cliente": val.get('nombre_cliente'),
-
+                "gama": val.get('gama'),
+                "nombre": val.get('nombre'),
+                "codigo_producto": val.get('codigo_producto'),
+                "precio_venta": val.get('precio_venta'),
             })
-           
+    return producto
+# Obtener el nombre del producto y la descripcion de este
+def getAllNombreDescripcion():
+    descripcion = []
+    for val in pr.producto:
+        descripcion.append({
+            "nombre": val.get('nombre'),
+            "descripcion": val.get('descripcion'),
+            "codigo_producto": val.get('codigo_producto'),
+        })
+    return descripcion
+# Obtener el proveedor del producto y precio
+def getAllProveedorPrecio():
+    proveedor = []
+    for val in pr.producto:
+        proveedor.append({
+            "codigo_producto": val.get('codigo_producto'),
+            "nombre": val.get('nombre'),
+            "proveedor": val.get('proveedor'),
+            "precio_proveedor": val.get('precio_proveedor'),
+        })
+    return proveedor
+# Obtener el precio por fabrica y el precio neto individual
+def getAllPrecioNetoAVenta():
+    Precios = []
+    for val in pr.producto:
+        Precios.append({
+            "nombre": val.get('nombre'),
+            "codigo_producto": val.get('codigo_producto'),
+            "precio_proveedor": val.get('precio_proveedor'),
+            "precio_venta": val.get('precio_venta'),
 
+        })
+    return Precios
 
 
 def menu():
@@ -40,13 +73,39 @@ def menu():
           /_/                                                             /_/                                                 
                             0.  Regresar al menu principal
                             1. Obtener un listado con los productos q pertenecen a la gama ornamentales
+                            2. Obtener la gama, el nombre del cliente y el codigo de cliente
+                            3. Obtener el nombre del producto y la descripcion de este
+                            4. Obtener el proveedor del producto y precio
+                            5. Obtener el precio por fabrica y el precio neto individual
         """)
     opcion = int(input("\nSeleccione una de las opciones: "))
     if(opcion == 1):
-        gama = str(input("Introduce una gama"))
-        stock = int(input("Introduce las unidades en stock"))
-        print(tabulate(getAllStocksPriceGama(gama, stock), headers="keys", tablefmt="grid"))
-    
+        try:
+            gama = str(input("Introduce una gama"))
+            stock = int(input("Introduce las unidades en stock"))
+            print(tabulate(getAllStocksPriceGama(gama, stock), headers="keys", tablefmt="grid"))
+        except KeyboardInterrupt:
+            return menu()    
+    elif(opcion == 2):
+        try:
+            print(tabulate(getAllGamaCodigoNombre(), headers="keys", tablefmt="github"))
+        except KeyboardInterrupt:
+            return menu()
+    elif(opcion == 3):
+        try:
+              print(tabulate(getAllNombreDescripcion(), headers="keys", tablefmt="github"))
+        except KeyboardInterrupt:
+            return menu()
+    elif(opcion == 4):
+        try:
+              print(tabulate(getAllProveedorPrecio(), headers="keys", tablefmt="github"))
+        except KeyboardInterrupt:
+            return menu()    
+    elif(opcion == 5):
+        try:
+              print(tabulate(getAllPrecioNetoAVenta(), headers="keys", tablefmt="github"))
+        except KeyboardInterrupt:
+            return menu()       
     elif(opcion == 0):
         break
     try:
