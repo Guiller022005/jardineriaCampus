039b370b2a8a7812bfb0f656195c7fe8cbd44c1f
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-03-14 09:13:34 -0500

    agreagar productos

--- modules/postProducto.py
+++ modules/postProducto.py
@@ -1,7 +1,50 @@
+import os
+from tabulate import tabulate
 import json
 import requests
-def postProducto(producto):
-    peticion = requests.post("http://172.16.100.123:50001", data=json.dumps(producto))
-    res = peticion.json()
-    return res
+import modules.getGamas as gG
 
+def postProducto():
+    # json-server storage/producto.json -b 50001
+    producto = {
+            "codigo_producto": input("Ingrese el codigo del producto: "),
+            "nombre": input("Ingrese el nombre del producto: "),
+            "gama": gG.getAllNombre()[int(input("Seleccione la gama:\n"+"".join([f"\t{i}. {val}\n" for i, val in enumerate(gG.getAllNombre())])))],
+            "dimensiones": input("Ingrese las dimensiones del producto: "),
+            "proveedor": input("Ingrese el proveedor del producto: "),
+            "descripcion": input("Ingrese la descripcion del producto: "),
+            "cantidad_en_stock": int(input("Ingrese la cantidad en stock: ")),
+            "precio_venta": int(input("Ingrese el precio de ventas: ")),
+            "precio_proveedor": int(input("Ingrese el precio del proveedor: "))
+}
+    headers = {'Content-type': 'application/json', 'charset': 'UTF-8'}
+    peticion = requests.post("http://172.16.100.113:50001",headers=headers, data=json.dumps(producto, indent=4).encode("UTF-8"))
+    res = peticion.json()
+    res["Mensaje"] = "Producto Guardado"
+    return [res]
+
+def menu():
+    while True:
+        os.system("clear")
+        print("""  
+    ___       __          _       _      __                         __      __                    __        
+   /   | ____/ /___ ___  (_)___  (_)____/ /__________ ______   ____/ /___ _/ /_____  _____   ____/ /__      
+  / /| |/ __  / __ `__ \/ / __ \/ / ___/ __/ ___/ __ `/ ___/  / __  / __ `/ __/ __ \/ ___/  / __  / _ \     
+ / ___ / /_/ / / / / / / / / / / (__  ) /_/ /  / /_/ / /     / /_/ / /_/ / /_/ /_/ (__  )  / /_/ /  __/     
+/_/  |_\__,_/_/ /_/ /_/_/_/ /_/_/____/\__/_/   \__,_/_/      \__,_/\__,_/\__/\____/____/   \__,_/\___/      
+    ____  ____  ____  ____  __  __________________  _____                                                   
+   / __ \/ __ \/ __ \/ __ \/ / / / ____/_  __/ __ \/ ___/                                                   
+  / /_/ / /_/ / / / / / / / / / / /     / / / / / /\__ \                                                    
+ / ____/ _, _/ /_/ / /_/ / /_/ / /___  / / / /_/ /___/ /                                                    
+/_/   /_/ |_|\____/_____/\____/\____/ /_/  \____//____/                                                     
+                                                                                                                                                    
+            1. Guardar un producto nuevo
+            0. Atras
+            
+        """)
+        opcion = int(input("\nSeleccione una de las opciones: "))
+        if(opcion == 1):
+           
+            print(tabulate(postProducto(), headers="keys", tablefmt="github"))
+            input("Presione una tecla para continuar......")
+            break
